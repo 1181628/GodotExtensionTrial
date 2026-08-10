073882015705f98b0a251084ea4c345b6d0a9986
1181628 <AppleWang1181628@gmail.com>
2026-08-10 14:19:08 +1200

    v3

--- MyGodotExtension/src/player.cpp
+++ MyGodotExtension/src/player.cpp
@@ -1,15 +1,13 @@
-//读取 player.h
 #include "player.h"
 
 #include <godot_cpp/variant/vector2.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/classes/input.hpp>
+#include <godot_cpp/classes/engine.hpp>
 
 using namespace godot;
 
 Player::Player() {
-	// 相当于 GDScript 中创建变量时设置：
-	// var gravity = 580
-	gravity = 580.0;
 }
 
 Player::~Player() {
@@ -19,22 +17,31 @@ void Player::_bind_methods() {
 }
 
 void Player::_ready() {
-	// 相当于 GDScript 中的 pass
 }
 
-void Player::_process(double delta) {
-	// 目前没有使用 delta，防止编译器产生警告
-	(void)delta;
 
-	// 取得 CharacterBody2D 自带的 velocity
-	Vector2 velocity = get_velocity();
+void Player::_physics_process(double delta) {
+    if (Engine::get_singleton()->is_editor_hint()) {
+        return;
+    }
 
-	// 相当于 GDScript：velocity.y = 10
-	velocity.y = 10.0;
+    Input *input = Input::get_singleton();
+    Vector2 velocity = get_velocity();
 
-	// 将修改后的 velocity 放回 CharacterBody2D
-	set_velocity(velocity);
+    // gravity
+    if (!is_on_floor()) {
+        velocity.y += gravity * delta;
+    }
 
-	// 相当于 GDScript：move_and_slide()
-	move_and_slide();
+    // jump
+    if (input->is_action_just_pressed("jump") && is_on_floor()) {
+        velocity.y = vertical_acceleration;
+    }
+
+    // move left and right
+    double moveVector_x = input->get_axis("move_left", "move_right");
+    velocity.x += moveVector_x * horizontal_acceleration * delta;
+
+    set_velocity(velocity);
+    move_and_slide();
 }
