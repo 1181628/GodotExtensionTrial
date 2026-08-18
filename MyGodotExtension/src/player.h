6df52f081378de2e47698b43600334e606b51a3f
1181628 <AppleWang1181628@gmail.com>
2026-08-18 14:21:25 +1200

    6

--- MyGodotExtension/src/player.h
+++ MyGodotExtension/src/player.h
@@ -3,17 +3,43 @@
 
 #include <godot_cpp/classes/character_body2d.hpp>
 #include <godot_cpp/variant/vector2.hpp>
+#include <godot_cpp/classes/animation_player.hpp>
 
 namespace godot {
 
 class Player : public CharacterBody2D {
-	GDCLASS(Player, CharacterBody2D)
+	GDCLASS(Player, CharacterBody2D);
 
 private:
 	double gravity = 580.0;
     double horizontal_acceleration = 2000.0;
 	double maxHorizontalSpeed = 120.0;
-	double jumpSpeed = 250.0;
+	double jumpSpeed = 210.0;
+
+	// all possible player states
+	enum class State {
+        NORMAL,
+        DASH,
+        ATTACK,
+        ATTACK_UP,
+        ATTACK_DOWN
+    };
+	// record the player's initial state
+	State current_state = State::NORMAL;
+
+	// Records whether the player has just entered a new state
+    bool is_state_new = true;
+
+	void change_state(int new_state);
+
+	void process_normal(double delta);
+    void process_dash(double delta);
+    void process_attack(double delta);
+    void process_attack_up(double delta);
+    void process_attack_down(double delta);
+
+	// Changes the player's animation
+    void _update_animation();
 
 protected:
 	static void _bind_methods();
@@ -22,11 +48,8 @@ public:
 	Player();
 	~Player();
 
-	// Godot callback functions
 	void _ready() override;
 	void _physics_process(double delta) override;
-	// Custom action functions
-	void _update_animation();
 };
 
 }
