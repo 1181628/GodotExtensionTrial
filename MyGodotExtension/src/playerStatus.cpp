643c150fd021e6a80e9b8f7b4116cf48412cabab
1181628 <AppleWang1181628@gmail.com>
2026-09-02 12:36:49 +1200

    enemy1 2

---
+++ MyGodotExtension/src/playerStatus.cpp
@@ -0,0 +1,59 @@
+#include "playerStatus.h"
+
+#include <godot_cpp/classes/animation_player.hpp>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/variant/string.hpp>
+#include <godot_cpp/classes/engine.hpp>
+
+using namespace godot;
+
+PlayerStatus::PlayerStatus() {
+}
+
+PlayerStatus::~PlayerStatus() {
+}
+
+void PlayerStatus::_bind_methods() {
+}
+
+void PlayerStatus::_ready() {
+    // Prevents gameplay code from running inside the editor
+    if (Engine::get_singleton()->is_editor_hint()) {
+        return;
+    }
+
+    playerStatusValue = 4;
+    // Displays the starting health bar animation
+    refresh_player_status();
+}
+
+void PlayerStatus::refresh_player_status() {
+    AnimationPlayer *animationPlayer = get_node<AnimationPlayer>("AnimationPlayer");
+
+    // Plays an animation matching the current health value
+    if (playerStatusValue == 0) {
+        animationPlayer->play("0");
+    }
+    if (playerStatusValue == 1) {
+        animationPlayer->play("1");
+    }
+    if (playerStatusValue == 2) {
+        animationPlayer->play("2");
+    }
+    if (playerStatusValue == 3) {
+        animationPlayer->play("3");
+    }
+    if (playerStatusValue == 4) {
+        animationPlayer->play("4");
+    }
+    // Prevents the health value from going below 0
+    if (playerStatusValue < 0) {
+        playerStatusValue = 0;
+        animationPlayer->play("0");
+    }
+    // Prevents the health value from going above 4
+    if (playerStatusValue > 4) {
+        playerStatusValue = 4;
+        animationPlayer->play("4");
+    }
+}
