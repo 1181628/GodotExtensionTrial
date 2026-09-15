40e055b26b22a97e7112248ce17cdbab4ff572a6
1181628 <AppleWang1181628@gmail.com>
2026-09-16 10:27:47 +1200

    v19

--- MyGodotExtension/src/gameCamera.cpp
+++ MyGodotExtension/src/gameCamera.cpp
@@ -1,5 +1,8 @@
 #include "gameCamera.h"
 
+#include <godot_cpp/classes/engine.hpp>
+#include <godot_cpp/classes/node2d.hpp>
+#include <godot_cpp/classes/scene_tree.hpp>
 #include <godot_cpp/core/math.hpp>
 #include <godot_cpp/variant/vector2.hpp>
 
@@ -17,9 +20,34 @@ void GameCamera::_bind_methods() {
 }
 
 void GameCamera::_ready() {
+    cameraStartPosition = get_global_position();
 }
 
 void GameCamera::_process(double delta) {
+    // Find the player.
+    Node2D *player = Object::cast_to<Node2D>(get_tree()->get_first_node_in_group("player"));
+
+    Vector2 cameraPosition = get_global_position();
+    Vector2 playerPosition = player->get_global_position();
+
+
+
+    float distanceX = player->get_global_position().x
+                    - cameraStartPosition.x;
+
+    // Follow slightly, within 10 units of the starting position.
+    float offsetX = Math::clamp(distanceX * 0.1f, -10.0f, 10.0f);
+    float targetX = cameraStartPosition.x + offsetX;
+
+    float weight = 1.0f - Math::exp(-2.0f * float(delta));
+
+    cameraPosition.x = Math::lerp(cameraPosition.x, targetX, weight);
+    cameraPosition.y = cameraStartPosition.y;
+
+    set_global_position(cameraPosition);
+
+
+
     // Creates a random position between -strength and strength
     double randomX = -strength + (static_cast<double>(rand()) / RAND_MAX) * strength * 2;
     double randomY = -strength + (static_cast<double>(rand()) / RAND_MAX) * strength * 2;
